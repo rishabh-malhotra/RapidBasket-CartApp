1c4d99071a2c7eb8a8986983d3096062291ffbbd
rishabh-malhotra <rishabh11.rm@gmail.com>
2020-05-16 22:03:33 +0530

    added navbar and some other changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import NavbarComponent from './components/Shared/NavbarComponent';
 
+const TITLE="Rapid Basket";
 function App() {
+  document.title=TITLE;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React latest version
-        </a>
-      </header>
-    </div>
+    <NavbarComponent />
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const TITLE="RapidBasket";
 ReactDOM.render(
   <React.StrictMode>
     <App />
